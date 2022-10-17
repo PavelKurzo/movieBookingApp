6f53eb769d3b1f381d4c097f531485e4716465da
Pavel <pavel.kurzo@bereyziat.dev>
2022-10-17 16:07:28 +0200

    Ticket View

---
+++ MovieBookingApp/MovieBookingApp/MovieBookingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieBookingAppApp.swift
+//  MovieBookingApp
+//
+//  Created by Павел Курзо on 17.10.22.
+//
+
+import SwiftUI
+
+@main
+struct MovieBookingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
